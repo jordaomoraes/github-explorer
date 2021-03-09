d28c4a0962405d513778eb4824db1ca0d919f378
Guilherme Jordão <>
2021-03-08 22:10:11 -0300

    Configuracoes Iniciais Completas

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,26 @@
+export function RepositoryList(){
+  return (
+    <section className="respository-list">
+      <h1> Lista de repositorios</h1>
+      <ul>
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in react</p>
+          <a href=""> Acessar respositorio</a>
+        </li>
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in react</p>
+          <a href=""> Acessar respositorio</a>
+        </li>
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in react</p>
+          <a href=""> Acessar respositorio</a>
+        </li>
+      </ul>
+    </section>
+  )
+
+
+}
