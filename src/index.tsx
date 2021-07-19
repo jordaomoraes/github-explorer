a348e7ce73ee47a0b239eed79bb5df81f3e7c98b
Guilherme Jordão <>
2021-07-19 20:34:41 -0300

    detalhes

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import {render} from 'react-dom'
 
 import {App} from './App'
 
+//AQUI O QUE VAI EXIBIDO, NO SEGUNDO PARAMETRO É AONDE VAI SER EXIBIDO
 render(<App />, document.getElementById('root'))
 
